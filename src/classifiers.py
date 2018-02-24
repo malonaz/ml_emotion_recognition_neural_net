8e03473144a8e538dd580d3a53c1a541d6986563
Malon Azria <ma12917@vm-shell3.doc.ic.ac.uk>
2018-02-24 17:52:07 +0000

    implemented loss

--- src/classifiers.py
+++ src/classifiers.py
@@ -15,19 +15,31 @@ def softmax(logits, y):
     - loss: Loss scalar
     - dlogits: Loss gradient with respect to logits
     """
-    loss, dlogits = None, None
-    """
-    TODO: Compute the softmax loss and its gradient using no explicit loops
-    Store the loss in loss and the gradient in dW. If you are not careful
-    here, it is easy to run into numeric instability. Don't forget the
-    normalisation!
-    """
-    ###########################################################################
-    #                           BEGIN OF YOUR CODE                            #
-    ###########################################################################
+    
+    # compute K = -max(Wx_i), which will be used for numerical stability.
+    # here since we are dealing with logs, we will compute logK directly for each row.
+    logKs = logits.max(axis = 1).reshape(logits.shape[0], 1)
+    
+    # add the logKs to the appropriate rows to stabilize the logits
+    # for each entry, raise the number e to the power of this entry to get the score
+    stableScores = np.exp(logits + logKs)    
 
+    # collapse matrix horizontally to get the sum of the scores of each sample
+    sampleScoreSums = stableScores.sum(axis = 1).reshape(logits.shape[0], 1)
 
-    ###########################################################################
-    #                            END OF YOUR CODE                             #
-    ###########################################################################
+    # for each example, divide each class score by the sum of the scores of all classes
+    # this turns the scores into a probability distribution over the classes: P(y_i = class_j|logits)
+    classDistribution = stableScores / sampleScoreSums
+
+    # compute likelihood: P(y_i = correctClass | logits)
+    correctClassProbabilities = classDistribution[np.arange(classDistribution.shape[0]), y]
+
+    # compute the negative log-likelihood 
+    negLogLikelihood = -np.log(correctClassProbabilities)
+
+    # loss = sigma(negloglikelihood)/N
+    loss = negLogLikelihood.sum()/negLogLikelihood.shape[0]
+    
+    dlogits = None
+    
     return loss, dlogits
