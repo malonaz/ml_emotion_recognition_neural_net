6e10c2cc8195269a094bbdd6c66dc5af2d8868c6
Malon Azria <ma12917@matrix05.doc.ic.ac.uk>
2018-02-26 15:22:08 +0000

    gather data

--- src/overfit_fcnet.py
+++ src/overfit_fcnet.py
@@ -8,7 +8,10 @@ from src.utils.data_utils import get_CIFAR10_data
 DIR = "datasets/cifar-10-batches-py/"
 
 # contains the filenames of the 5 dataset batches
-BATCHES_FILENAMES = map(lambda x: DIR + "data_batch" + str(x + 1), range(5))
+BATCHES_FILENAMES = [DIR + "data_batch_" + str(x + 1) for x in range(5)]
+
+# contains the filename of the batches's meta file
+BATCH_META_FILENAME = DIR + "batches.meta"
 
 def unpickle(filename):
     """
@@ -30,8 +33,16 @@ def unpickle(filename):
     return obj
 
 
-# contains the unpickled batches
-batches = map(lambda filename: unpickle(filename), BATCHES_FILENAMES)
+# unpicle batch meta info into a list
+batch_meta = unpickle(BATCH_META_FILENAME)
+examples_per_batch, class_names, num_vis = batch_meta.values() 
+
+# unpickled batches into a list
+batches = [unpickle(filename) for filename in BATCHES_FILENAMES]
+
+# extract examples and data from batches, in tuples (examples, labels)
+data = [(batch[list(batch.keys())[2]], batch[list(batch.keys())[1]]) for batch in batches]
+
 
 overtfit_fcnet = FullyConnectedNet([10,10,10] , input_dim=32*32*3, num_classes=10,
                                    dropout=0, reg=0.0, weight_scale=1e-2, dtype=np.float32,
