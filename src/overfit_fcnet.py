aba79a7a28ef81fe73c44ef7c5f92f8364cdd488
Malon Azria <ma12917@matrix01.doc.ic.ac.uk>
2018-02-26 23:39:30 +0000

    added function to pickle overfit net

--- src/overfit_fcnet.py
+++ src/overfit_fcnet.py
@@ -5,48 +5,56 @@ from src.fcnet import FullyConnectedNet
 from src.utils.solver import Solver
 from src.utils.data_utils import get_CIFAR10_data
 
-# get CIFAR10 data
-""" keys of data
-      'X_train': X_train, 'y_train': y_train,
-      'X_val': X_val, 'y_val': y_val,
-      'X_test': X_test, 'y_test': y_test,
-"""
-data = get_CIFAR10_data()
+import pickle
 
 
-# intialize net
-model = FullyConnectedNet([200, 150] , input_dim=32*32*3, num_classes=10,
-                          dropout=0, reg=0.0, weight_scale=1e-2, dtype=np.float32,
-                          seed = 1)
+def train_overfit_net(plot = True):
+    
+    # get CIFAR10 data
+    data = get_CIFAR10_data()
+    
 
-solver = Solver(model, data,
-                update_rule='sgd',
-                optim_config={
-                    'learning_rate': 5e-2,
-                },
+    # intialize net
+    model = FullyConnectedNet([10, 10] , input_dim=32*32*3, num_classes=10,
+                              dropout=0, reg=0.0, weight_scale=1e-2, dtype=np.float32,
+                              seed = 1)
+
+    # initialize solver
+    solver = Solver(model, data,
+                    update_rule='sgd',
+                    optim_config={
+                        'learning_rate': 5e-3,
+                    },
                 lr_decay=0.95,
-                num_epochs=20, batch_size=100,
-                print_every=100)
-x = data["X_test"]
+                    num_epochs=1, batch_size=100,
+                    print_every=100)
+    
+    # train the net 
+    solver.train()
+
+    # pickle the solver
+    pickle.dump(solver, open("nets/overfit_net.p", "wb"))
+    
+    if plot:
+
+        plt.subplot(2, 1, 1)
+
+        plt.subplot(2, 1, 1)
+    
+        plt.title("trainingloss")
+        plt.plot(solver.loss_history, "o")
+        plt.xlabel('Iteration')
+        
+        plt.subplot(2, 1, 2)
+        plt.title('Accuracy')
+        plt.plot(solver.train_acc_history,'-o', label = 'train')
+        plt.plot(solver.val_acc_history,'-o', label = 'val')
+        plt.plot([0.5] * len(solver.val_acc_history), 'k--')
+        plt.xlabel('Epoch')
+        plt.legend(loc = 'lowerright')
+        plt.gcf().set_size_inches(15, 12)
+        plt.show()
 
 
-solver.train()
-
-plt.subplot(2, 1, 1)
-
-plt.subplot(2, 1, 1)
-
-plt.title("trainingloss")
-plt.plot(solver.loss_history, "o")
-plt.xlabel('Iteration')
-
-plt.subplot(2, 1, 2)
-plt.title('Accuracy')
-plt.plot(solver.train_acc_history,'-o', label = 'train')
-plt.plot(solver.val_acc_history,'-o', label = 'val')
-plt.plot([0.5] * len(solver.val_acc_history), 'k--')
-plt.xlabel('Epoch')
-plt.legend(loc = 'lowerright')
-plt.gcf().set_size_inches(15, 12)
-plt.show()
+train_overfit_net(False)
 
