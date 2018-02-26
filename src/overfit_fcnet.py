889edd4cdc79ac2424a15d9d69e8ae2e758d834f
themalonaz <malonazria@gmail.com>
2018-02-26 20:51:21 +0000

    fixed import statement

--- src/overfit_fcnet.py
+++ src/overfit_fcnet.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from src.fcnet import FullyConnectedNet
 from src.utils.solver import Solver
@@ -14,7 +15,7 @@ data = get_CIFAR10_data()
 
 
 # intialize net
-overtfit_fcnet = FullyConnectedNet([10,10,10] , input_dim=32*32*3, num_classes=10,
-                                   dropout=0, reg=0.0, weight_scale=1e-2, dtype=np.float32,
-                                   seed = 1)
-
+model = FullyConnectedNet([10,10,10] , input_dim=32*32*3, num_classes=10,
+                          dropout=0, reg=0.0, weight_scale=1e-2, dtype=np.float32,
+                          seed = 1)
+solver = Solver(model, data)
