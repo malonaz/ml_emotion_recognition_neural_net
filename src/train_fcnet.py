f729223dd5103c3e34d4a2ccc4ec8c2e73e9814d
themalonaz <malonazria@gmail.com>
2018-02-27 12:21:53 +0000

    added lab environment files

--- src/train_fcnet.py
+++ src/train_fcnet.py
@@ -8,10 +8,3 @@ from src.utils.data_utils import get_CIFAR10_data
 TODO: Use a Solver instance to train a TwoLayerNet that achieves at least 50% 
 accuracy on the validation set.
 """
-###########################################################################
-#                           BEGIN OF YOUR CODE                            #
-###########################################################################
-
-##############################################################################
-#                             END OF YOUR CODE                               #
-##############################################################################
