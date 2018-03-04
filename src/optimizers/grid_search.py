90667ead7872da1eb3bd2d588fe7f3f476c5a84b
themalonaz <malonazria@gmail.com>
2018-03-04 15:47:35 +0000

    added optimize L2 regularization rate

--- src/optimizers/grid_search.py
+++ src/optimizers/grid_search.py
@@ -10,7 +10,7 @@ from src.net_trainer import train_net
 fer2013_data = load_data()
 
 # defaut parameters
-default_learning_rate = 1e-3
+default_learning_rate = 6.5e-5
 default_num_classes = 7
 default_momentum = 0.9
 default_hidden_units = 256
@@ -106,15 +106,13 @@ def get_optimal_learning_rate_info():
     save_net_info("nets/optimal_learning_rate", solver)
     
 
-        
-        
-def optimize_momentum():
+def optimize_dropout():
 
     # define item being optimized
-    optim_param = "momentum"
+    optim_param = "dropout rate"
     
     # define filename template
-    filename = "src/optimizers/outputs/grid_search/momentum.txt"
+    filename = "src/optimizers/outputs/grid_search/dropout_rate.txt"
     
     # define range on optim variable, and initiate it
     start  = 0.0
@@ -123,64 +121,19 @@ def optimize_momentum():
     optim_variable = start
     
     # append optimization info to file
-    title = "Momentum optimizer with 256 hidden units and momentum 0.5 \n"
-    csv_format = "momentum, best validation rate accuracy\n"
+    title = "Dropout rate optimizer with 256 hidden units and momentum 0.5 \n"
+    csv_format = "dropout rate, best validation rate accuracy\n"
     append_to_file(filename, title + csv_format)
-
+    
     # start optim
     for i in range(num_iterations):
-
+        
         # create and train a net
         solver = train_net(data            = fer2013_data,
                            num_classes     = default_num_classes,
                            hidden_dims     = [default_hidden_units],
                            input_dim       = 48 * 48 * 3,
-                           learning_rate   = default_learning_rate,
-                           update_rule     = default_update_rule,
-                           momentum        = optim_variable,
-                           num_epochs      = default_num_epochs,
-                           batch_size      = default_batch_size,
-                           lr_decay        = default_lr_decay)
-        
-        # append results of iteration
-        result = str.format('{0:6f}', optim_variable) + ", " + str.format('{0:6f}', solver.best_val_acc) + "\n"
-        append_to_file(filename, result)
-        
-        # increment learning rate
-        optim_variable += incr
-            
-    # generate plot of optimization
-    plot_data(filename, title, "grid_search")
-
-
-def optimize_hidden_units():
-
-    # define item being optimized
-    optim_param = "hidden units"
-    
-    # define filename template
-    filename = "src/optimizers/outputs/grid_search/hidden_units.txt"
-    
-    # define range on optim variable, and initiate it
-    start  = 50
-    end = 2550
-    incr = (end - start)/num_iterations
-    optim_variable = start
-    
-    # append optimization info to file
-    title = "Hidden units optimizer with 256 hidden units and momentum 0.5 \n"
-    csv_format = "hidden units, best validation rate accuracy\n"
-    append_to_file(filename, title + csv_format)
-
-    # start optim
-    for i in range(num_iterations):
-
-
-        # create and train a net
-        solver = train_net(data            = fer2013_data,
-                           num_classes     = default_num_classes,
-                           hidden_dims     = [int(optim_variable)],
-                           input_dim       = 48 * 48 * 3,
+                           dropout         = optim_variable,
                            learning_rate   = default_learning_rate,
                            update_rule     = default_update_rule,
                            momentum        = default_momentum,
@@ -188,15 +141,60 @@ def optimize_hidden_units():
                            batch_size      = default_batch_size,
                            lr_decay        = default_lr_decay)
 
+
         # append results of iteration
         result = str.format('{0:6f}', optim_variable) + ", " + str.format('{0:6f}', solver.best_val_acc) + "\n"
         append_to_file(filename, result)
         
         # increment learning rate
         optim_variable += incr
-            
-    # generate plot of optimization
-    plot_data(filename, title, "grid_search")
+
+
+        
+def optimize_L2():
+
+    # define item being optimized
+    optim_param = "L2 regularization rate"
+    
+    # define filename template
+    filename = "src/optimizers/outputs/grid_search/L2_rate.txt"
+    
+    # define range on optim variable, and initiate it
+    start  = 0.0
+    end = 1.0
+    incr = (end - start)/num_iterations
+    optim_variable = start
+    
+    # append optimization info to file
+    title = "L2 regularization rate optimizer with 256 hidden units and momentum 0.9 \n"
+    csv_format = "L2 rate, best validation rate accuracy\n"
+    append_to_file(filename, title + csv_format)
+    
+    # start optim
+    for i in range(num_iterations):
+        
+        # create and train a net
+        solver = train_net(data            = fer2013_data,
+                           num_classes     = default_num_classes,
+                           hidden_dims     = [default_hidden_units],
+                           input_dim       = 48 * 48 * 3,
+                           reg             = optim_variable,
+                           learning_rate   = default_learning_rate,
+                           update_rule     = default_update_rule,
+                           momentum        = default_momentum,
+                           num_epochs      = default_num_epochs,
+                           batch_size      = default_batch_size,
+                           lr_decay        = default_lr_decay)
+
+
+        # append results of iteration
+        result = str.format('{0:6f}', optim_variable) + ", " + str.format('{0:6f}', solver.best_val_acc) + "\n"
+        append_to_file(filename, result)
+        
+        # increment learning rate
+        optim_variable += incr
+
+        
 
 
 def generate_plots():
@@ -218,13 +216,6 @@ def generate_plots():
     plot_data(filename, title, "grid_search")
 
 
-def optimize():
-    optimize_learning_rate()
-    #optimize_momentum()
-    #optimize_hidden_units()
 
-#optimize()
-#generate_plots()    
-
-
-get_optimal_learning_rate_info()
+generate_plots()
+#optimize_dropout()
