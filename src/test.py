34c569d6a72a52776db5c4a874d14c718a43227b
themalonaz <malonazria@gmail.com>
2018-03-04 13:48:26 +0000

    added test fer model skeleton

---
+++ src/test.py
@@ -0,0 +1,18 @@
+def test_fer_model(img_folder, model = "nets/fer2013_net/pickled_net.p"):
+    """
+    Given a folder with images, load the images (in lexico-graphical ordering
+    according toe the filename of the images) and your best model to predict
+    the facial expression of each image.
+
+    args:
+    - image_folder: Path to the images to be tested
+    - model: Path to the pickled model
+
+    returns:
+    - preds: A numpy vector of size N with N being the number of images in img_folder.
+    """
+
+
+    preds = None
+
+    return preds
