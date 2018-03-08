d3d848181545eb84f895b552060b6cbc2757da97
themalonaz <malonazria@gmail.com>
2018-03-08 19:22:25 +0000

    added more layers

--- src/cnn.py
+++ src/cnn.py
@@ -1,43 +1,49 @@
 from keras.models import Sequential
-from keras.layers import Dense, Convolution2D, MaxPooling2D, Dropout, Activation, Flatten
-from keras.optimizers import SGD, Adadelta
+from keras.layers import Dense, MaxPooling2D, Dropout, Activation, Flatten, Conv2D, AveragePooling2D
+from keras.optimizers import Adam
 from keras.utils import np_utils
 from keras.regularizers import l2
 from src.utils.data_utils import *
 from keras.preprocessing.image import ImageDataGenerator
+
+# DATASET PARAMETERS
+img_rows, img_cols = 48, 48
+batch_size  = 128
+nb_classes = 7
+nb_epoch = 10
+img_channel = 3
 
 def get_new_model():
     
     # declare model
     model = Sequential()
 
-    ### ADD LAYERS
-    # add a 2d convolution layer, with valid padding. make sure channels come first
-    model.add(Convolution2D(kernel_size = 5, strides = 1, data_format = "channels_first",
-                        filters = 64, padding = 'valid', input_shape = (1, 48, 48)))
-    
-    # add a relu layer. input will be shape = (None, 64, 44, 44)
-    model.add(Dense(units = 512, activation='relu', use_bias = False))
-    
-    # add a 2 x 2 max pool layer
-    model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+    # FIRST LAYER
+    model. add(Conv2D(64, (5, 5), activation = 'relu', data_format = "channels_first", input_shape = (1, img_rows, img_cols)))
+    model.add(MaxPooling2D(pool_size = (5, 5), strides = (2, 2)))
 
-    # add an affine + relu
-    model.add(Dense(units = 512, activation = 'relu', use_bias = True))
+    # SECOND LAYER
+    model.add(Conv2D(64, (3, 3), activation = 'relu'))
+    model.add(Conv2D(64, (3, 3), activation = 'relu'))
+    model.add(AveragePooling2D(pool_size = (3, 3), strides = (2, 2)))
 
-    # flatten then add output layer with softmax activaiton
+    # THIRD LAYER
+    model.add(Conv2D(128, (3, 3), activation = 'relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(AveragePooling2D(pool_size=(3,3), strides=(2, 2)))
+
     model.add(Flatten())
-    model.add(Dense(7, activation = 'softmax'))
 
+    # FCC
+    model.add(Dense(1024, activation = 'relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(1024, activation = 'relu'))
+    model.add(Dropout(0.2))
 
-    
-    # define optimizer
-    sgd = SGD(lr = 0.01,
-              momentum = 0.9,
-              decay = 0.95)
-    
+    model.add(Dense(nb_classes, activation = 'softmax'))
+        
     model.compile(loss = 'categorical_crossentropy',
-                  optimizer = sgd,
+                  optimizer = Adam(),
                   metrics = ['accuracy'])
     
     
@@ -47,11 +53,6 @@ def get_new_model():
 
 
 
-img_rows, img_cols = 48, 48
-batch_size  = 128
-nb_classes = 7
-nb_epoch = 1200
-img_channel = 3
 
 
 # get data
@@ -72,14 +73,17 @@ X_val = X_val.reshape(X_val.shape[0], 1, img_rows, img_cols)
 y_train = np_utils.to_categorical(y_train, nb_classes)
 y_val = np_utils.to_categorical(y_val, nb_classes)
 
+# create generators
+gen = ImageDataGenerator()
+
+train_generator = gen.flow(X_train, y_train, batch_size = batch_size)
 
 # get a new model
 model = get_new_model()
 
-model.fit(X_train,
-          y_train,
-          epochs = 5,
-          batch_size = 32,
-          validation_data = (X_val, y_val),
-          verbose = 1)
+model.fit_generator(train_generator,
+                    steps_per_epoch = batch_size,
+                    epochs = nb_epoch,
+                    verbose = 1)
+                    
 
